a8757d11a5989cecd0410b2569e5069c703faabf
brajpatel <brajeshpatel0002@gmail.com>
2023-08-12 14:49:33 +0100

    Get developer and all games by author

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -1,4 +1,5 @@
 const Developer = require('../models/developer');
+const Game = require('../models/game');
 const asyncHandler = require('express-async-handler');
 
 exports.developer_list = asyncHandler(async (req, res, next) => {
@@ -8,7 +9,18 @@ exports.developer_list = asyncHandler(async (req, res, next) => {
 })
 
 exports.developer_detail = asyncHandler(async (req, res, next) => {
-    res.send(`TO DO: Developer Detail: ${req.params.id}`);;
+    const [developer, allGamesByDeveloper] = await Promise.all([
+        Developer.findById(req.params.id).exec(),
+        Game.find({ developer: req.params.id }, "name ").exec()
+    ]);
+
+    if(!developer) {
+        const err = new Error("Developer not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("developer_detail", { title: developer.name, developer: developer });
 })
 
 exports.developer_create_get = asyncHandler(async (req, res, next) => {
