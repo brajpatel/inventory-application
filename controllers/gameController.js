135e32f315129266e28e4858faf613609e3c9397
brajpatel <brajeshpatel0002@gmail.com>
2023-08-17 15:36:45 +0100

    Find game by id and update

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -190,9 +190,120 @@ exports.game_update_get = asyncHandler(async (req, res, next) => {
     })
 })
 
-exports.game_update_post = asyncHandler(async (req, res, next) => {
-    res.send("TO DO: Game Update POST");
-})
+exports.game_update_post = [
+    (req, res, next) => {
+        if(!(req.body.platform instanceof Array)) {
+          if(typeof req.body.platform === "undefined") {
+            req.body.platform = [];
+          }
+          else {
+            req.body.platform = new Array(req.body.platform)
+          }
+        }
+
+        if(!(req.body.genre instanceof Array)) {
+          if(typeof req.body.genre === "undefined") {
+            req.body.genre = [];
+          }
+          else {
+            req.body.genre = new Array(req.body.genre)
+          }
+        }
+  
+        next();
+      },
+      body("name", "Game name must contain at least 3 characters")
+          .trim()
+          .isLength({ min: 3 })
+          .escape(),
+      body("description", "Game description must contain at least 20 characters")
+          .trim()
+          .isLength({ min: 20 })
+          .escape(),
+      body("price", "Price must be a value greater than or equal to 0")
+          .trim()
+          .escape(),
+      body("number_in_stock", "Number in stock must be a value greater than or equal to 0")
+          .trim()
+          .escape(),
+      body("initial_release_date", "Initial release date must not be empty")
+          .optional({ values: "falsy" })
+          .isISO8601()
+          .toDate(),
+      body("developer", "A developer must be selected")
+          .trim()
+          .escape(),
+      body("platform.*")
+          .escape(),
+      body("genre.*")
+          .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        let filename = 'default-image.jpg';
+
+        if(!(req.file === undefined)) {
+            filename = req.file.filename;
+        }
+
+        const game = new Game({
+            name: req.body.name,
+            release_date: req.body.release_date,
+            description: req.body.description,
+            price: req.body.price,
+            number_in_stock: req.body.number_in_stock,
+            developer: req.body.developer,
+            platform: req.body.platform,
+            genre: req.body.genre,
+            image: filename,
+            _id: req.params.id,
+            errors: errors.array()
+        })
+
+        if(!errors.isEmpty()) {
+            const [allDevelopers, allPlatforms, allGenres] = await Promise.all([
+                Developer.find().exec(),
+                Platform.find().exec(),
+                Genre.find().exec()
+            ])
+            
+            for(const platform of allPlatforms) {
+                if(game.platform.indexOf(platform._id) > -1) {
+                    platform.checked = "true";
+                }
+            }
+
+            for(const genre of allGenres) {
+                if(game.genre.indexOf(genre._id) > -1) {
+                    genre.checked = "true";
+                }
+            }
+
+            res.render("game_form", {
+                title: "Create Game",
+                game: game,
+                developers: allDevelopers,
+                platforms: allPlatforms,
+                genres: allGenres,
+                errors: errors.array()
+            });
+
+            return;
+        }
+        else {
+            const gameExists = await Game.findOne({ name: req.body.name }).collation({ locale: 'en', strength: 2 }).exec();
+            
+            if(gameExists) {
+                res.redirect(gameExists.url);
+            }
+            else {
+                const updatedGame = await Game.findByIdAndUpdate(req.params.id, game, {});
+                res.redirect(updatedGame.url);
+            }
+        }
+    })
+]
 
 exports.game_delete_get = asyncHandler(async (req, res, next) => {
     const game = await Game.findById(req.params.id).exec();
@@ -209,5 +320,5 @@ exports.game_delete_get = asyncHandler(async (req, res, next) => {
 
 exports.game_delete_post = asyncHandler(async (req, res, next) => {
     await Game.findByIdAndRemove(req.body.gameid);
-    res.redirect("/collection/games");dddddddddddddddd
+    res.redirect("/collection/games");
 })
